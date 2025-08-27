0ca863b8c145a2ec44590b3b3b37cba1b2b6e1c8
arjit321 <arjit_sharma@canalys.com>
2025-08-27 18:40:15 +0530

    fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,22 +18,7 @@ const blogCollection = defineCollection({
   }),
 });
 
-// Pages collection schema
-const pagesCollection = defineCollection({
-  schema: z.object({
-    id: z.string().optional(),
-    title: z.string(),
-    meta_title: z.string().optional(),
-    description: z.string().optional(),
-    image: z.string().optional(),
-    layout: z.string().optional(),
-    draft: z.boolean().optional(),
-  }),
-});
-
-
 // Export collections
 export const collections = {
-  articles: blogCollection,
-  pages: pagesCollection
+  articles: blogCollection
 };
