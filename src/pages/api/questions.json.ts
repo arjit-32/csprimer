be1253e49b6dae239f43cd87d1431298c8b1214b
arjit321 <arjit_sharma@canalys.com>
2026-08-31 00:04:35 +0530

    star question functionality bug fix

--- src/pages/api/questions.json.ts
+++ src/pages/api/questions.json.ts
@@ -1,12 +1,14 @@
 import { getCollection } from 'astro:content';
+import { markdownify } from '@/lib/utils/textConverter';
 
 export async function GET() {
   const allQuestionFiles = await getCollection('interview-questions');
   const questions = allQuestionFiles.flatMap(file => 
     file.data.questions.map(q => ({
+      id: q.id,
       question: q.question,
       tags: q.tags,
-      answer: q.answer,
+      answer: Array.isArray(q.answer) ? q.answer.map(a => markdownify(a)) : markdownify(q.answer),
       code: q.code || null,
       images: q.related_images || null
     }))
